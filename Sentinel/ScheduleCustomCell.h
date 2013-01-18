4de2e94930127e725bdef60c6cd3ee5e4598a317
Adam Mitha <adam.mitha@gmail.com>
2013-01-18 03:52:31 -0800

    added schedule custom cell

---
+++ Sentinel/ScheduleCustomCell.h
@@ -0,0 +1,13 @@
+//
+//  ScheduleCustomCell.h
+//  Sentinel
+//
+//  Created by Adam Mitha on 2013-01-18.
+//  Copyright (c) 2013 Sentinel Secondary School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScheduleCustomCell : UITableViewCell
+
+@end
---
+++ Sentinel/ScheduleCustomCell.m
@@ -0,0 +1,29 @@
+//
+//  ScheduleCustomCell.m
+//  Sentinel
+//
+//  Created by Adam Mitha on 2013-01-18.
+//  Copyright (c) 2013 Sentinel Secondary School. All rights reserved.
+//
+
+#import "ScheduleCustomCell.h"
+
+@implementation ScheduleCustomCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
